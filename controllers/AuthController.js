158b6101e1bf2ebbbeb977eb8c9344685584d144
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-04-29 17:21:45 -0300

    validando usuario e redirecionando para a home

---
+++ controllers/AuthController.js
@@ -0,0 +1,54 @@
+const { Usuario } = require('../models');
+//const Usuario = require('../models/Usuario');   é igual o de cima
+const bcrypt = require('bcrypt');
+
+const AuthController = {
+    
+    showLogin: (req,res) => {
+        //req.query.error    pra tratar o error = 1
+        res.render('auth/login');
+    },
+
+    showRegistro: (req,res) => {
+        res.render('auth/register');
+    },
+
+    showHome: (req,res) => {
+        console.log(req.session.usuario);
+        res.render('index');
+    },
+
+    login: async (req, res) => {
+        //res.send(req.body);
+
+        //lendo as informações do body
+        const { email, senha } = req.body;
+
+        //tentar carregar um usuario
+        const user = await Usuario.findOne({ where: { email } });
+        //const user = await Usuario.findOne({ where: { email: email}}   igual o de cima
+        //console.log(user);
+        //res.send(user);
+
+        //verifica se existe usuario com o email passado
+        if(!user){
+            res.redirect('/login?error=1');   //redirect sempre manda pro get
+
+        }
+
+        //validar a senha passada via post contra a guardada no banco
+        if (!bcrypt.compareSync(senha, user.senha)){
+            res.redirect('/login?error=1');
+        }
+
+        //setar uma session com o usuário
+        req.session.usuario = user;
+        
+        //redirecionar (redirecionar sempre get) o usuário para a rota '/home'
+        res.redirect("/home");
+    }
+
+
+}
+
+module.exports = AuthController;
